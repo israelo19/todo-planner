85bc3a3906a3586e3c7b6ec054d911d5180cb8d6
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2025-01-15 01:58:23 -0500

    2nd Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,30 @@
-// filepath: /Users/israelogwu/Library/Mobile Documents/com~apple~CloudDocs/Documents/VSCode Projects/Todo/todo-planner/src/components/Home.js
 import React from 'react';
-import List from './List'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Your To-DO List:</h1>
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="text-center mb-8">
+        <h1 className="font-Jersey25 text-[#152E4D] flex flex-col items-center">
+          <span className="text-9xl mb-4 drop-shadow-[0_1.2px_1.2px_rgba(5,0,0,0.8)] relative animate-bounce animation-delay-0">
+            What do
+          </span>
+          <span className="text-9xl mb-4 drop-shadow-[0_1.2px_1.2px_rgba(5,0,0,0.8)] relative animate-bounce animation-delay-0">
+            you have
+          </span>
+          <span className="text-9xl drop-shadow-[0_1.2px_1.2px_rgba(5,0,0,0.8)] relative animate-bounce animation-delay-0">
+            ToDO?
+          </span>
+        </h1>
+      </div>
       
-      <List></List>
+      <div className="flex flex-col gap-4 w-full max-w-xs">
+        <button className="bg-[#152E4D] text-white py-3 px-6 w-full text-2xl font-Jersey25">
+          Login
+        </button>
+        <button className="bg-[#152E4D] text-white py-3 px-6 w-full text-2xl font-Jersey25">
+          Sign Up!
+        </button>
+      </div>
     </div>
   );
 };
